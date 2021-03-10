2b56026f5712ff8438c8f6ecdfd5f4d09fc467a1
Blair <bbrownlieedward@gmail.com>
2021-03-09 19:56:13 -0700

    adding some test api files

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const app = express()
 
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.listen(port, function() {
+app.get('/api', (req, res) => {
+    res.sendFile()
+})
+
+app.listen(port, function () {
     console.log(`listening on port:${port}`)
 })
